1032da83c692c32e8dfa4eccc357f0b19ff608f3
Nicolas <nicolas.yang.1@gmail.com>
2020-04-02 10:03:01 -0300

    Change display content. Add async. Add hovering div.

--- js/script.js
+++ js/script.js
@@ -1,88 +1,31 @@
 const container = document.querySelector('#container');
 const content = document.createElement('div');
 
-let createSquare = () => {
-    for (i = 0; i < 16 * 16; i++) {
+
+
+// creation grille 16x16 divs carrés  + survol des divs
+
+let createSquare = (quantity = 16) => {
+    for (i = 0; i < quantity ** 2; i++) {
         const content = document.createElement('div');
-        content.setAttribute('style', 'margin: 0.1rem; background: #222; width: 4rem; height: 4rem')
         content.classList.add('square')
-        container.appendChild(content);
+        content.setAttribute('style', 'display: grid; margin: 0.1rem; background: #666; width: 4rem; height: 4rem')
+        content.addEventListener("mouseenter", (e) => {
+            e.target.style.background = "red"
+        })
+        container.appendChild(content)
     }
 }
-
-container.setAttribute('style', 'display: flex; justify-content:center; flex-direction: column; flex-wrap: wrap')
-
+container.setAttribute('style', 'display: grid;')
 createSquare()
 
-
-
-
-/*
-//Global variables//
-const container = document.querySelector("#container");
-const resizebtn = document.querySelector("#resize");
-const gridLines = document.documentElement;
-const cells = document.getElementsByClassName("cell");
-
-
-//Create default grid//
-function createCells(quantity = 16) {
-    for (let i = 0; i < quantity ** 2; ++i) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("cell");
-        container.appendChild(newDiv);
-    }
-}
-
-
-//Create color using random hexadecimal value//
+// Changement de couleur
 function createColor() {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-
-//Color cells if empty, otherwise decreases their brightness//
-function cellColor() {
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener("mouseover", (e) => {
-            let brightness = getComputedStyle(e.target).getPropertyValue("filter").split(/\(([^)]+)\)/);
-            if (brightness == "none") {
-                e.target.style.backgroundColor = createColor();
-                e.target.style.filter = "brightness(1)";
-            } else if (brightness[1] > 0) {
-                e.target.style.filter = `brightness(${brightness[1] - 0.2})`;
-            }
-        })
-    }
-}
+// Bouton pour restet la grille
 
 
-//Clear cells inline code to reset their color//
-function cellClear() {
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener("click", () => {
-            cells[i].removeAttribute("style")
-        })
-    }
-}
 
-
-//Create grid based on user input and re-initialize the app//
-resizebtn.addEventListener("click", () => {
-    container.innerHTML = ""
-    const quantity = prompt("Set the grid using a value between 2 and 64.");
-    if (quantity < 2 || quantity > 64) {
-        quantity = prompt("Please insert a value from 2 to 64.")
-    }
-    createCells(quantity);
-    gridLines.style.setProperty("--quantity", quantity)
-    cellColor();
-    cellClear();
-})
-
-
-//Initialize the app//
-createCells();
-cellColor();
-cellClear();
-*/
+/*
